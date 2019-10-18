568aaf93e42113cdaf83b286731690ad5b40a946
Kenneth Zheng <ken_zhe@msn.com>
2019-10-17 23:08:15 -0500

    Set 1 challenge 6 solved

--- set1/challenge6.py
+++ set1/challenge6.py
@@ -2,6 +2,10 @@ import base64
 from challenge3 import charFreqScore, singleCharXor
 from challenge5 import repeatingKeyXor
 
+"""Attempts to break repeating key XOR in a text document.
+Cryptopals set 1 challenge 6.
+Last modified: October 17, 2019."""
+
 def oneCounter(n):
     """Computes the number of ones in the binary representation
     of the integer n."""
@@ -11,10 +15,11 @@ def oneCounter(n):
     return (1 & n) + oneCounter(n >> 1)
 
 def binEditDistance(b1, b2):
-    """Returns the edit distance between two bytes objects."""
+    """Returns the binary edit distance between two bytes objects."""
     if len(b1) > len(b2):
         return binEditDistance(b2, b1)
 
+    # the number of ones in b1 ^ b2 is the binary edit distance
     numOfOnes = 0
     for i in range(len(b1)):
         numOfOnes += oneCounter(b1[i] ^ b2[i])
@@ -24,33 +29,35 @@ def binEditDistance(b1, b2):
 
     return numOfOnes
 
-def keySizeScore(inputBytes, keySize, n=1):
-    """Scores a keySize from the ciphertext inputBytes using the first n chucks of length keySize."""
-    #total = sum([binEditDistance(inputBytes[keySize * i : keySize * (i + 1)], inputBytes[keySize * (i + 1) : keySize * (i + 2)]) for i in range(n)])
-    total = 0
-    for i in range(n):
-        total += binEditDistance(inputBytes[keySize * i : keySize * (i + 1)], inputBytes[keySize * (i + 1) : keySize * (i + 2)])
-    #return total / (n * keySize) 
-    return total / (keySize) 
+def keySizeScore(inputBytes, keySize):
+    """Scores a keySize from the ciphertext inputBytes length keySize chunks from inputBytes."""
+    chunks = splitBytes(inputBytes, keySize)
+    averages = []
+    for i in range(0, len(chunks), 2):
+        if i + 1 < len(chunks):
+            averages.append(binEditDistance(chunks[i], chunks[i + 1]) / keySize)
+    return sum(averages) / len(averages)
 
-#def bestKeySize(inputBytes, lo, hi):
-#    bestScore = hi + 1
-#    bestSize = hi + 1
-#    for size in range(lo, hi + 1):
-#        score = keySizeScore(inputBytes, size)
-#        if score < bestScore:
-#            bestScore = score
-#            bestSize = size
-#    return bestSize
+def bestKeySize(inputBytes, lo, hi):
+    """Returns the best key size in range lo to hi (inclusive)."""
+    bestScore = hi + 1
+    bestSize = hi + 1
+    for size in range(lo, hi + 1):
+        score = keySizeScore(inputBytes, size)
+        if score < bestScore:
+            bestScore = score
+            bestSize = size
+    return bestSize
 
-def bestKeySizes(inputBytes, lo, hi):
+def keySizes(inputBytes, lo, hi):
+    """Returns a set of tuples (score, keySize) of key sizes."""
     scores = set()
     for size in range(lo, hi + 1):
         scores.add((keySizeScore(inputBytes, size), size))
     return scores
 
 def grabBytes(filename):
-    """Returns the contents of the file in a byte array. Assumes the file contents
+    """Returns the contents of the file in bytes object. Assumes the file contents
     is encoded in base 64."""
 
     f = open(filename, "r")
@@ -60,6 +67,8 @@ def grabBytes(filename):
     return base64.b64decode(contents)
 
 def decipherSingleCharXor(inputBytes):
+    """Attempts to brute force decipher inputBytes using single character XOR and English
+    character frequencies."""
     bestScore = -1
     bestChar = 0
     bestDecipher = b""
@@ -76,13 +85,15 @@ def decipherSingleCharXor(inputBytes):
     return (bestDecipher, bestChar)
 
 def splitBytes(inputBytes, length):
-    """Returns a list of bytes objects."""
+    """Splits inputBytes into a list of bytes objects. Each bytes object in the list
+    is of size length."""
     outputList = []
     for i in range(0, len(inputBytes), length):
         outputList.append(inputBytes[i : i + length])
     return outputList
 
 def transpose(bytesList, length):
+    """Transposes a list of bytes."""
     outputList = []
     for i in range(length):
         outputList.append(b"")
@@ -93,68 +104,18 @@ def transpose(bytesList, length):
 
 if __name__ == "__main__":
     data = grabBytes("6.txt")
-
-    #keySize = bestKeySize(data, 2, 40)
-    keySizes = bestKeySizes(data, 2, 40)
-
-    #test = bytes.fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
-    #(d, c) = decipherSingleCharXor(test)
-    #print(c)
-
-    #test = splitBytes(b"This is a test of the split bytes function!", 3)
-    #print(transpose(test, 3))
-
-    keySize = 29 # TODO Fix keySize computation
+    keySize = bestKeySize(data, 2, 40)
+    
     splitData = splitBytes(data, keySize)
     transData = transpose(splitData, keySize)
+
     key = b""
     for inputBytes in transData:
         (d, c) = decipherSingleCharXor(inputBytes)
         key += chr(c).encode()
-    print(key)
+    print("This is the key:")
+    print(key.decode())
 
     deciphered = repeatingKeyXor(data, key)
+    print("\nThis is the deciphered text:")
     print(deciphered.decode())
-
-
-   # i = 0
-   # keys = []
-   # for score, keySize in keySizes:
-   #     if i == 5:
-   #         break
-   #     i += 1
-
-   #     splitData = splitBytes(data, keySize)
-   #     transData = transpose(splitData, keySize)
-
-   #     key = b""
-   #     for inputBytes in transData:
-   #         (d, c) = decipherSingleCharXor(inputBytes)
-   #         key += chr(c).encode()
-   #     keys.append(key)
-
-   # #print(keys)
-   # decipheredList = []
-   # for key in keys:
-   #     deciphered = repeatingKeyXor(data, key)
-   #     decipheredList.append(deciphered)
-   # print(decipheredList[3])
-
-
-
-
-    #splitData = splitBytes(data, keySize)
-    #transData = transpose(splitData, keySize)
-
-    #key = ""
-    #for inputBytes in transData:
-    #    (d, c) = decipherSingleCharXor(inputBytes)
-    #    key += (chr(c))
-    #    print(inputBytes)
-    #    print(c)
-#    print(key)
-#    print(data[:2 * keySize])
-#    print(splitData[0])
-#    print(splitData[1])
-#    print(splitData[2])
-#    print(transData[0])
